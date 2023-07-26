20d1791f30943d2e0233b5d92ec1277a6a8357b3
nguyễn quang sơn <70741367+vincestone133@users.noreply.github.com>
2023-07-26 15:56:04 +0700

    Stable 1 3:55

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Portfolio 2023</title>
+        <meta name="description" content="Designed by Nguyen Quang Son" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
